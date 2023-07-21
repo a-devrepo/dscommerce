c2b6ba8d151e7d074881ad5cd9e11bee1aa09d75
alison <adevpr11@gmail.com>
2023-07-21 12:21:41 -0300

    Payment

--- src/main/java/com/devsuperior/dscommerce/entities/Order.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -20,6 +20,9 @@ public class Order {
   @JoinColumn(name = "client_id")
   private User client;
 
+  @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+  private Payment payment;
+
   public Order() {}
 
   public Order(Long id, Instant moment, OrderStatus status, User client) {
@@ -27,6 +30,14 @@ public class Order {
     this.moment = moment;
     this.status = status;
     this.client = client;
+  }
+
+  public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+    this.id = id;
+    this.moment = moment;
+    this.status = status;
+    this.client = client;
+    this.payment = payment;
   }
 
   public Long getId() {
