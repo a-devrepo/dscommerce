a29f1991d2c498dbdaa24f8789ebabfb925a1abc
alison <adevpr11@gmail.com>
2023-07-21 15:41:27 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -28,6 +28,9 @@ public class Product {
       inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
 
+  @OneToMany(mappedBy = "id.product")
+  private Set<OrderItem> items = new HashSet<>();
+
   public Product() {}
 
   public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -80,5 +83,13 @@ public class Product {
 
   public Set<Category> getCategories() {
     return categories;
+  }
+
+  public Set<OrderItem> getItems() {
+    return items;
+  }
+
+  public List<Order> getOrders() {
+    return items.stream().map(x -> x.getOrder()).toList();
   }
 }
