b23386e471e11b344cf969cde47f66737cb21214
alison <adevpr11@gmail.com>
2023-07-26 22:36:11 -0300

    Validation fix

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -4,10 +4,7 @@ import com.devsuperior.dscommerce.entities.Category;
 import com.devsuperior.dscommerce.entities.Product;
 import java.util.ArrayList;
 import java.util.List;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class ProductDTO {
   private Long id;
@@ -20,6 +17,7 @@ public class ProductDTO {
   @NotBlank(message = "Campo obrigatório")
   private String description;
 
+  @NotNull(message = "Campo obrigatório")
   @Positive(message = "O preço deve ser positivo")
   private Double price;
 
--- src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ValidationError.java
@@ -17,6 +17,7 @@ public class ValidationError extends CustomError {
   }
 
   public void addError(String fieldName, String message) {
+    errors.removeIf(x -> x.getFieldMessage().equals(fieldName));
     errors.add(new FieldMessage(fieldName, message));
   }
 }
